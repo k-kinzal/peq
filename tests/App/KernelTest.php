c30c507e96b95dfe66a5eda688fe87a7cc5ca928
k-kinzal <keen.flag7803@logn.in>
2025-03-30 14:16:11 +0900

    fixes migrate phpunit conf

--- tests/App/KernelTest.php
+++ tests/App/KernelTest.php
@@ -2,4 +2,12 @@
 
 namespace Tests\App;
 
+use PHPUnit\Framework\TestCase;
 
+class KernelTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
